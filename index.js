15f3ce3fb9e559871fd44304b8f5c8b199e8b68b
bartmk <31205578+bartmk@users.noreply.github.com>
2026-02-21 17:01:16 +0000

    4

--- index.js
+++ index.js
@@ -3,15 +3,11 @@ const { Resend } = require('resend');
 
 async function main() {
   try {
-    const apiUrl = process.env.API_URL || 'https://api-test.pte.nu/api/v2/my_profile';
+    const apiUrl = process.env.API_URL;
     const apiKey = process.env.API_KEY;
     const resendApiKey = process.env.RESEND_API_KEY;
     const emailTo = process.env.EMAIL_TO;
-    const emailFrom = process.env.EMAIL_FROM || 'onboarding@resend.dev';
-
-    if (!apiKey) throw new Error('API_KEY nie jest ustawiony');
-    if (!resendApiKey) throw new Error('RESEND_API_KEY nie jest ustawiony');
-    if (!emailTo) throw new Error('EMAIL_TO nie jest ustawiony');
+    const emailFrom = process.env.EMAIL_FROM;
 
     const response = await axios.get(apiUrl, {
       headers: {
@@ -20,19 +16,17 @@ async function main() {
     });
     const { free_leech } = response?.data?.details || {};
 
-    console.log('Odpowiedź z API:', response?.data?.details);
     const resend = new Resend(resendApiKey);
 
     await resend.emails.send({
         from: emailFrom,
         to: emailTo,
         subject: 'Free leech available',
-        html: `<p>Free leech działa</p>`
+        html: `<p>Free leech available</p>`
     });
 
-    console.log('Mail wysłany przez Resend');
   } catch (error) {
-    console.error('Błąd:', error);
+    console.error('Error:', error);
   }
 }
 
